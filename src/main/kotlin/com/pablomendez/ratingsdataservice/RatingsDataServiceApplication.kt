b67e10f0590f93870b1c5b8f8058ef47fbe83e5e
Pablo Mendez <mendez.developer@gmail.com>
2020-08-04 21:50:32 -0300

    initial commit

---
+++ src/main/kotlin/com/pablomendez/ratingsdataservice/RatingsDataServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.pablomendez.ratingsdataservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RatingsDataServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RatingsDataServiceApplication>(*args)
+}
